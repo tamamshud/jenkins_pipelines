d4cd959a0cc38d5fc59fde13b77005de89e8c27f
Dmytro Didenko1 <dmytro_didenko1@epam.com>
2020-04-01 14:42:16 +0000

    2

---
+++ vars/stageGitCheckout.groovy
@@ -0,0 +1,20 @@
+def call (Closure body) {
+    stage ('GitCheckout') {
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+        
+        checkout([$class: 'GitSCM',
+            branches: [[name: "${config.branchName}"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[$class: 'RelativeTargetDirectory',
+                relativeTargetDir: "${config.remoteDir}"]],
+            submoduleCfg: [],
+            userRemoteConfigs: [[
+                credentialsId: "${config.credentialsID}" , 
+                url: "${config.gitRepository}" 
+            ]]
+        ])
+    }
+}
