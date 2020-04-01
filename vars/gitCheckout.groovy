4918c377435a755cfc0658a9082b595d7c38078a
Dmytro Didenko1 <dmytro_didenko1@epam.com>
2020-04-01 14:23:20 +0000

    2

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,5 @@
 def call (Closure body) {
     stage ('GitCheckout') {
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
 
 checkout([$class: 'GitSCM', 
 	branches: [[name: "${config.branchName}"]], 
