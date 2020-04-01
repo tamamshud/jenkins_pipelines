1e4843333c132e3c65a63657e3e5f0257f3ddb82
Dmytro Didenko1 <dmytro_didenko1@epam.com>
2020-04-01 17:55:56 +0000

    2

--- vars/stageParallelTests.groovy
+++ vars/stageParallelTests.groovy
@@ -4,12 +4,11 @@ def call(Closure body) {
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()
-	steps {
            parallel 
                 performance test: { sh "${config.performanceCommand}" }
                 regression test: { sh "${config.regressionCommand}" }
                 integration test: { sh "${config.integrationCommand}" }
           
-       }
+       
    }
 }  
