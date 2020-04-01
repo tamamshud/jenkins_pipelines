d6c02b4bebb89e1cb61d8a529a57c35e83cec0e7
Dmytro Didenko1 <dmytro_didenko1@epam.com>
2020-04-01 14:16:32 +0000

    test

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,17 +4,13 @@ def call (Closure body) {
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()
-        
-        checkout([$class: 'GitSCM',
-            branches: [[name: "${config.branchName}"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [[$class: 'RelativeTargetDirectory',
-                relativeTargetDir: "${config.remoteDir}"]],
-            submoduleCfg: [],
-            userRemoteConfigs: [[
-                credentialsId: "${config.credentialsID}" , 
-                url: "${config.gitRepository}" 
-            ]]
-        ])
+
+checkout([$class: 'GitSCM', 
+	branches: [[name: "${config.branchName}"]], 
+	doGenerateSubmoduleConfigurations: false, 
+	extensions: [],
+	submoduleCfg: [],
+	userRemoteConfigs: [[credentialsId: 'GitHub_User',
+	url: 'https://github.com/tamamshud/test-maven-project']]])
     }
 }
